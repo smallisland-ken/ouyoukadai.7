312156aa71c7db77a28c1a1a126116a5e5f9a254
smallisland_ken <kojima0519@gmail.com>
2021-07-13 14:54:29 +0000

    Assignment3 second push

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+     user_path(current_user.id)
+  end
+
+  def after_sign_out_path_for(resource)
+    "https://5e66cbfdc0a14e1fb14b28962a845f21.vfs.cloud9.us-east-1.amazonaws.com/users/sign_in"
+  end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
 end
