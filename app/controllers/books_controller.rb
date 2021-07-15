4269aeef2b8abf00997a9e3ca0f19b16257144b7
smallisland_ken <kojima0519@gmail.com>
2021-07-15 13:05:39 +0000

    Assignment3 sixth push

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,21 @@
 class BooksController < ApplicationController
 
-  def show
+  def new
+    @book = Book.new
+  end
 
+  def show
+    @book = Book.find(params[:id])
   end
 
   def index
-    end
+  end
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     @book.save
-    redirect_to book_path(current_user.id)
+    redirect_to book_path(@book)
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,28 @@
 class UsersController < ApplicationController
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user.params)
-    @user = user.save
-    redirect_to_book_path(current_user.id)
-  end
-
   def show
+    @book = Book.new
     @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def index
     @book = Book.new
+    # 空のインスタンス変数なのでnewは可能。
     @users = User.all
     @user = User.find(current_user.id)
   end
 
   def edit
-   end
+   @user = User.find(params[:id])
+   redirect_to_book_path(current_user.id)
+  end
 
   private
   def list_params
-    params.require(:list).permit(:title, :body, :image)  end
+    params.require(:list).permit(:title, :body, :image)
+  end
+  def user_params
 
-
+  end
 end
