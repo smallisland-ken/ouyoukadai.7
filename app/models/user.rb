534d33922bd536daa004223c922f82c22983123a
smallisland_ken <kojima0519@gmail.com>
2021-08-03 05:38:39 +0000

    Ouyoukadai4 fourth push

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,14 @@ class User < ApplicationRecord
 
          has_many :books, dependent: :destroy
          has_many :favorites, dependent: :destroy
-         has_many :reverse_of_relationship, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+         has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
          has_many :followers, through: :reverse_of_relationships, source: :follower
-         has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy       
+         # 被フォロー関係を通じて参照→followed_idをフォローしている人
+        
+         has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+         # 【class_name: "Relationship"】は省略可能
          has_many :followings, through: :relationships, source: :followed
+        # 与フォロー関係を通じて参照→follower_idをフォローしている人
          has_many :book_comments, dependent: :destroy
          validates :name, length: { minimum: 2 }
          validates :name, length: { maximum: 20 }
