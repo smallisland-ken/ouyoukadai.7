7e54b6817e7deedeff5deee6aac1a8b0a132d58a
smallisland_ken <kojima0519@gmail.com>
2021-08-07 10:50:39 +0000

    ouyoukadai6 second push

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,19 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(post_comment_params)
+    # (post_comment_params)はストロングパラメータ
     # アソシエーションを使った場合にbook_commentsで記載ができる。
     # comment = BookComment.new(post_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     # comment.user_id = current_user.id
     comment.save
-    redirect_to book_path(book)
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
     # user_id: params[:id],これは必要ない。なぜかとbook_idを取得した時点でアソシエーションにより、bookモデル内にあるuser_idがあるため、今回の場合はbook_idを取得することで自動的にuserのidも取得できる。
-    redirect_back(fallback_location: root_path)
   end
   
   private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,15 @@
 class FavoritesController < ApplicationController
     def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
+    # favorite = current_user.favorites.new(book_id: book.id)
+    # book_id: book.idのままだと、どの本のidか不明なので、一つ前の行に記述してある@bookを使用すればそのまま本のidを引き渡せる。
     favorite.save
-    redirect_back(fallback_location: root_path)
     end
     
     def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
     end
 end
