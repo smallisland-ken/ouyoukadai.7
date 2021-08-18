e475a9afa492342d51937439ebf1409f35fa0dbb
smallisland_ken <kojima0519@gmail.com>
2021-08-18 13:16:17 +0000

    ouyoukadai8a fifth  push

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path
   end
-
-  protected
+ 
+   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
